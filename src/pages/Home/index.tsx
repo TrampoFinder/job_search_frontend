6b73b7022e3487de82f1fdb3640ebdc5d02479f7
devlarissa <devlarissasilva@gmail.com>
2025-03-14 19:12:39 -0300

    Added carousel with names and linkedins

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,19 @@
 import { BlackHeader } from "../../components/Header/BlackHeader";
 import briefcase from "../../assets/briefcase.svg";
 import enterprise from "../../assets/enterprise.svg";
-import nizos from "../../assets/nizos.svg";
+import linkedin from "../../assets/linkedin-logo.png";
 import { CardJob } from "../../components/ListBox/ListJobs/CardJob";
 import { BlackFooter } from "../../components/Footer";
 import { BgContentTop } from "../../components/Header/BgContentTop";
 import { useContext, useEffect, useState } from "react";
 import { JobManagementContext } from "../../contexts/JobContext";
 import { SearchByCompany } from "../../components/Form/SearchByCompany";
+import { useNavigate } from "react-router-dom";
 import './style.css';
 
 export const Home = () => {
+  const navigate = useNavigate();
+
   const { retrieveJobs, retrieveJobsCount, jobsCompanyCount, getJobsPagination } =
     useContext(JobManagementContext);
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -24,6 +27,26 @@ export const Home = () => {
     fetchData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+  
+  const [currentPerson, setCurrentPerson] = useState(0);
+
+  const linkLinkedIns = ["https://www.linkedin.com/in/andrewairamdasilva/", 
+    "https://www.linkedin.com/in/larissa-fabiana-ferreira/", 
+    "https://www.linkedin.com/in/nicole-xavier-sp/",
+    "https://www.linkedin.com/in/rodolfogueiros/", 
+    "https://www.linkedin.com/in/wesley-bueno/"];
+  const nomes = ["Andrew da Silva", "Larissa Silva", "Nicole Xavier", "Rodolfo Gueiros", "Wesley Bueno"];
+
+  const handleNext = () => {
+    setCurrentPerson((prev) => (prev + 1) % nomes.length);
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(handleNext, 3000); // A cada 3 segundos
+    return () => clearInterval(intervalId);
+  }, []);
+
+
   return (
     <div className="flex flex-col">
       <BgContentTop height="home">
@@ -71,25 +94,24 @@ export const Home = () => {
           </div>
         </main>
       </BgContentTop>
-      <footer className="bg-black w-full h-[100px]">
-        <div className="container-apply flex items-center h-full justify-between">
-          <img src={nizos} alt="Nizos" />
-          <img src={nizos} alt="Nizos" />
-          <img src={nizos} alt="Nizos" />
-          <img src={nizos} alt="Nizos" />
-          <img src={nizos} alt="Nizos" />
-        </div>
-      </footer>
+      
+      <div className="bg-black w-full h-[100px] flex justify-center">
+          <a href={linkLinkedIns[currentPerson]} className="flex items-center gap-5">
+            <img src={linkedin} alt="LinkedIn" className="h-[50px] w-[50px]" />
+            <h1 className="text-[70px] text-white">{nomes[currentPerson]}</h1>
+          </a>
+      </div>
+
       <div className="bg-white">
         <section className="container-apply">
           <div className="pt-[60px] flex justify-between">
             <div className="flex flex-col gap-[20px]">
               <h2 className="font-bold text-5xl">Vagas recomendadas</h2>
               <span>
-                At eu lobortis pretium tincidunt amet lacus ut aenean aliquet
+              Explore as oportunidades mais recentes e encontre a vaga ideal para você.
               </span>
               <div className="flex flex-col justify-end">
-                <a href="#" className="text-brand-1 underline">
+                <a href="" onClick={() => navigate("/sign-in")} className="text-brand-1 underline">
                   Ver todas
                 </a>
               </div>
